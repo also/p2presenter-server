a42b4126cd510d0903a13d9b0bfe72af482dfc0f
Ryan Berdeen <ryan@ryanberdeen.com>
2007-02-14 06:26:15 +0000

    fixed lecture create controller so courses property of lecture is set changed dispatcher servlet definition so it is loaded on startup
    git-svn-id: http://svn.p2presenter.org/p2presenter/trunk@118 a0511e01-a40d-46a4-a54e-97b21a75742a

--- src/java/edu/uoregon/cs/presenter/web/controller/instructor/LectureCreateController.java
+++ src/java/edu/uoregon/cs/presenter/web/controller/instructor/LectureCreateController.java
@@ -2,9 +2,6 @@
 
 package edu.uoregon.cs.presenter.web.controller.instructor;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.ServletRequestDataBinder;
@@ -12,28 +9,27 @@ import org.springframework.web.bind.ServletRequestUtils;
 
 import edu.uoregon.cs.presenter.entity.Course;
 import edu.uoregon.cs.presenter.entity.Lecture;
-import edu.uoregon.cs.presenter.entity.Person;
 import edu.uoregon.cs.presenter.web.controller.AbstractPresenterController;
 import edu.uoregon.cs.presenter.web.controller.AbstractPresenterSimpleFormController;
 
 public class LectureCreateController extends AbstractPresenterSimpleFormController {
 	private static String[] ALLOWED_FIELDS = {"title"};
-	public LectureCreateController() {
-		setCommandClass(Lecture.class);
+	
+	@Override
+	protected Object formBackingObject(HttpServletRequest request) throws Exception {
+		Lecture lecture = new Lecture();
+		
+		int courseId = ServletRequestUtils.getRequiredIntParameter(request, "courseId");
+		lecture.getCourses().add(getDao().loadEntity(Course.class, courseId));
+		
+		lecture.setCreator(AbstractPresenterController.getPerson(request));
+		
+		return lecture;
 	}
 	
 	@Override
 	protected void initBinder(HttpServletRequest request, ServletRequestDataBinder binder) throws Exception {
 		binder.setAllowedFields(ALLOWED_FIELDS);
-	}
-	
-	@Override
-	protected void onBind(HttpServletRequest request, Object command) throws Exception {
-		Lecture lecture = (Lecture) command;
-		Person person = AbstractPresenterController.getPerson(request);
-		lecture.setCreator(person);
-		Course course = (Course) request.getAttribute("course");
-		lecture.getCourses().add(course);
 	}
 	
 	@Override
@@ -44,17 +40,4 @@ public class LectureCreateController extends AbstractPresenterSimpleFormControll
 		// TODO maybe should add to course.lectures
 		flashMessage("lecture.created", new Object[] {lecture}, "lecture {0} created");
 	}
-
-	@Override
-	protected Map referenceData(HttpServletRequest request) throws Exception {
-		HashMap<String, Object> result = new HashMap<String, Object>();
-
-		int courseId = ServletRequestUtils.getRequiredIntParameter(request, "courseId");
-		result.put("course", getDao().loadEntity(Course.class, courseId));
-		
-		return result;
-	}
-	
-	
-
 }
