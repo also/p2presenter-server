f6fa49947a869e4024332c06faa57e23ce358a5c
Ryan Berdeen <ryan@ryanberdeen.com>
2007-02-16 12:45:44 +0000

    added simple create slide controller
    git-svn-id: http://svn.p2presenter.org/p2presenter/trunk@130 a0511e01-a40d-46a4-a54e-97b21a75742a

---
+++ src/java/edu/uoregon/cs/presenter/web/controller/instructor/SimpleNewSlideController.java
@@ -0,0 +1,30 @@
+/* $Id$ */
+
+package edu.uoregon.cs.presenter.web.controller.instructor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.uoregon.cs.presenter.entity.Lecture;
+import edu.uoregon.cs.presenter.entity.Slide;
+import edu.uoregon.cs.presenter.web.controller.AbstractPresenterController;
+
+public class SimpleNewSlideController extends AbstractPresenterController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		Lecture lecture = getDao().getEntity(Lecture.class, ServletRequestUtils.getRequiredIntParameter(request, "lectureId"));
+		Slide slide = new Slide();
+		slide.setIndex(lecture.getSlides().size());
+		lecture.getSlides().add(slide);
+		slide.setLecture(lecture);
+		getDao().save(slide);
+		getDao().flush();
+		ModelAndView result = new ModelAndView(getViewName());
+		return result;
+	}
+
+}
